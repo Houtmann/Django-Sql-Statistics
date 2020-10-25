6ef78cbed0a32e9dd66d1572425471daf75bb693
hhoutmann <hhoutmann@gmail.com>
2020-10-25 15:41:53 +0100

    add show result

--- management/commands/sql_stats.py
+++ management/commands/sql_stats.py
@@ -1,3 +1,5 @@
+from typing import OrderedDict
+
 from django.core.cache import cache
 from django.core.management.base import BaseCommand, CommandError
 
@@ -7,6 +9,16 @@ def delete_all() -> int:
     for key in cache_keys:
         cache.delete(key)
     return len(cache_keys)
+
+
+def show_all(order: str):
+    cache_keys: [str] = cache.keys("sql-stat:*")
+    res: OrderedDict = cache.get_many(cache_keys)
+    for line in res.items():
+        print(line)
+
+
+
 
 class Command(BaseCommand):
     help = 'Show sql Statistics'
@@ -18,19 +30,21 @@ class Command(BaseCommand):
             action='store_true',
             help='delete all data',
         )
+
         parser.add_argument(
-            '--delete',
+            '--show',
             action='store_true',
-            help='delete all data',
+            help='print data in console',
         )
 
-        #parser.add_argument('poll_ids', nargs='+', type=int)
-
     def handle(self, *args, **options):
-        delete = options.get("delete")
+        show: bool = options.get("show")
+        order: str = options.get("order")
+        delete: bool = options.get("delete")
+
+        if show:
+            show_all(order)
 
         if delete:
             nb_deleted_keys = delete_all()
             self.stdout.write(self.style.SUCCESS(f"Successfully delete {nb_deleted_keys} keys"))
-
-
